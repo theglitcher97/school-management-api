9346b3f78b640c74b78713f2ad58f0d4ac109777
TheGlitcher <forozcomercado@gmail.com>
2025-09-09 18:30:57 -0500

    Initial project config and database conexion

---
+++ src/main/java/com/school_management/api/SchoolManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.school_management.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/school_management/api/SchoolManagementSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school_management.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
